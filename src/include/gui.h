3b2b8e590a1e943f316c1f01b139c84c8a75af7e
NyxOn485 <kowalik.marcin81@gmail.com>
2025-04-11 19:26:57 +0200

    Update gui.h

--- src/include/gui.h
+++ src/include/gui.h
@@ -8,6 +8,9 @@ typedef struct {
     const char *label;
 } Button;
 
+char *file_list[MAX_FILES];
+int file_count = 0;
+
 void draw_button(Button *button);
 int is_button_clicked(Button *button, double mouse_x, double mouse_y);
 
